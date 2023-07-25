ab8fcf200de9bb7403c269162e3b5af1fd6dd26f
Mbktec <moubarak.yam@gmail.com>
2023-07-25 21:47:49 +0100

    Simple_Calculator_App Desktop

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator_Desktop
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			int First = Int32.Parse(txbFirstNumber.Text);
+			int Second = Int32.Parse(txbTwoNumber.Text);
+			lblResult_Som.Content = First + " +" + Second + " = " + (First + Second);
+			lblResult_Mul.Content = First + " *" + Second + " = " + (First * Second);
+			lblResult_Div.Content = First + " /" + Second + " = " + (First / Second);
+		}
+	}
+}
